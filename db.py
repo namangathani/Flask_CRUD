0292f8bc91975175c689c0988ed0b01ffab31b36
Naman Gathani <namangathani@gmail.com>
2024-04-02 19:56:00 +0530

    database added

---
+++ db.py
@@ -0,0 +1,4 @@
+from test import db, app
+with app.app_context():
+    db.create_all()
+exit()
--- test.py
+++ test.py
@@ -1,13 +1,27 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
 
+class Personal(db.Model):
+    name = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    def __repr__(self):
+        return "<Name: {}>".format(self.name)
+    
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.form:
-        print(request.form)
+        name = Personal(name=request.form.get("name"))
+        db.session.add(name)
+        db.session.commit()
     return render_template("homepage.html")
   
 if __name__ == "__main__":
