364132f9cba8bfdd84993685c4cc2b74b52fc7c2
Naman Gathani <namangathani@gmail.com>
2024-04-03 01:46:12 +0530

    displaying the data

--- test.py
+++ test.py
@@ -11,18 +11,20 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
 
-class Personal(db.Model):
-    name = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+class Food(db.Model):
+    item = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
     def __repr__(self):
-        return "<Name: {}>".format(self.name)
+        return "<item: {}>".format(self.item)
     
 @app.route("/", methods=["GET", "POST"])
 def home():
+    # names=None
     if request.form:
-        name = Personal(name=request.form.get("name"))
+        name = Food(item=request.form.get("item"))
         db.session.add(name)
         db.session.commit()
-    return render_template("homepage.html")
+    items = Food.query.all()    
+    return render_template("homepage.html",items=items)
   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
