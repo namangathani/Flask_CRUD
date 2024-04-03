d082cde30d8931c34c489d648c7bc09b019ce526
Naman Gathani <namangathani@gmail.com>
2024-04-03 13:21:11 +0530

    update

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask import redirect
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
@@ -25,6 +26,17 @@ def home():
         db.session.commit()
     items = Food.query.all()    
     return render_template("homepage.html",items=items)
-  
+
+@app.route("/update", methods=["POST"])
+def update():
+    new = request.form.get("new")
+    old = request.form.get("old")
+    item = Food.query.filter_by(item=old).first()
+    # item.name = new
+    if item:
+        item.item=new
+        db.session.commit()
+    return redirect("/")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
